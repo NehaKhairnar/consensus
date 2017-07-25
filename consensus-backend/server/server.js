bc9ba1955affc043ab470d55f5933e353eed040d
HetalRamani <hetal@qpair.io>
2017-07-25 15:34:18 -0400

    updated

--- consensus-backend/server/server.js
+++ consensus-backend/server/server.js
@@ -13,8 +13,14 @@ var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.options('*', cors())
+app.options('*', cors());
 app.use(cors());
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 const authCheck = jwt({
     secret: jwks.expressJwtSecret({
